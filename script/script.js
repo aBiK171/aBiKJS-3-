c6f0e54386ad8cda5c35df4627ddcd67ae48b581
aBiK <aBiK@gmail.com>
2020-01-26 22:06:05 +0300

    last version

---
+++ script/script.js
@@ -0,0 +1,84 @@
+var num = prompt('Введите время в цифрах')
+
+switch(num) {
+    
+    // УТРА
+
+    case '1':
+        alert('Один часа утра');
+        break;
+        case '2':
+        alert('Два часа утра');
+        break;
+        case '3':
+        alert('Три часа утра');
+        break;
+        case '4':
+        alert('Четыре часа утра');
+        break;
+        case '5':
+        alert('Пять часа утра');
+        break;
+        case '6':
+        alert('Шесть часа утра');
+        break;
+        case '7':
+        alert('Семь часа утра');
+        break;
+        case '8':
+        alert('Восемь часа утра');
+        break;
+        case '9':
+        alert('Девять часа утра');
+        break;
+        case '10':
+        alert('Десять часа утра');
+        break;
+        case '11':
+        alert('Одинадцать часа утра');
+        break;
+        case '12':
+        alert('Двенадцать часа утра');
+        break;
+
+
+    //   НОЧИ 
+
+        case '13':
+            alert('Один часа ночи');
+            break;
+            case '14':
+            alert('Два часа ночи');
+            break;
+            case '15':
+            alert('Три часа ночи');
+            break;
+            case '16':
+            alert('Четыре часа ночи');
+            break;
+            case '17':
+            alert('Пять часа ночи');
+            break;
+            case '18':
+            alert('Шесть часа ночи');
+            break;
+            case '19':
+            alert('Семь часа ночи');
+            break;
+            case '20':
+            alert('Восемь часа ночи');
+            break;
+            case '21':
+            alert('Девять часа ночи');
+            break;
+            case '22':
+            alert('Десять часа ночи');
+            break;
+            case '23':
+            alert('Одинадцать часа ночи');
+            break;
+            case '24':
+            alert('Двенадцать часа ночи');
+            break;
+        
+}
